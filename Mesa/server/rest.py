49af5569810f78aedc422fd57f7ba8cea3e34dde
LuisSalazar02 <luissalazar2002@hotmail.com>
2024-11-25 20:45:00 -0600

    Feat: implemented flask server

---
+++ Mesa/server/rest.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from ..city_model.model import CityModel
+from ..city_model.agents import CarAgent, SemaphoreAgent
+
+model = CityModel(5)
+app = Flask(__name__)
+
+@app.route("/positions")
+def get_positions():
+    data = {"agents":{}, "semaphores":{}}
+    for agent in model.agents_by_type[CarAgent]:
+        data["agents"][agent.unique_id] = {"coordinates": agent.pos}
+    for semaphore in model.agents_by_type[SemaphoreAgent]:
+        data["semaphores"][semaphore.id] = {"state": semaphore.state}
+    model.step()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host ='127.0.0.1', port = 8000, debug=True)
