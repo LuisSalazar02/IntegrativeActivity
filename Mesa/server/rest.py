87fe10e4b933be3c4587c0a41a11b91c9ddfc595
LuisSalazar02 <luissalazar2002@hotmail.com>
2024-11-27 15:28:13 -0600

    Feat: server sends data from just one endpoint

--- Mesa/server/rest.py
+++ Mesa/server/rest.py
@@ -1,36 +1,32 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify
+from flask_cors import CORS  # Importa CORS
 from ..city_model.model import CityModel
 from ..city_model.agents import CarAgent, SemaphoreAgent
 
 model = CityModel(5)
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Habilita CORS para todas las rutas
 
-last_state = []
-
-@app.route("/positions")
-def get_positions():
-    data = []
+@app.route("/step", methods=['GET'])
+def update_step():
+    # Obtener posiciones de los agentes tipo CarAgent
+    agents_data = []
     for agent in model.agents_by_type[CarAgent]:
-        data.append({"x": agent.pos[0], "z": agent.pos[1]})
+        agents_data.append({"x": agent.pos[0] * 10, "y": agent.pos[1] * 10})
+
+    # Obtener estados de los semáforos tipo SemaphoreAgent
+    semaphores_data = []
+    for semaphore in model.agents_by_type[SemaphoreAgent]:
+        semaphores_data.append(semaphore.state)
+
+    data = {
+        "agents": agents_data,
+        "semaphores": semaphores_data
+    }
+    
+    model.step()
+
     return jsonify(data)
 
-@app.route("/states")
-def get_states():
-    global last_state
-    data = []
-    for semaphore in model.agents_by_type[SemaphoreAgent]:
-        data.append({"state": semaphore.state})
-
-    if data != last_state:
-        last_state = data
-        return jsonify(data)
-    else:
-        return jsonify(None)
-
-@app.route("/step")
-def update_step():
-    model.step()
-    return Response(status=200)
-
 if __name__ == "__main__":
-    app.run(host ='127.0.0.1', port = 8000, debug=True)
+    app.run(host ='127.0.0.1', port=5000, debug=True)
